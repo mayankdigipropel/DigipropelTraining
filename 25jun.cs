3a3813d2ce38230ffa338cd23f82feac8758a0f1
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-06-25 10:25:10 +0530

    Add files via upload
    using main to call mymethod

---
+++ 25jun.cs
@@ -0,0 +1,17 @@
+using System;
+
+
+{
+  class Program
+  {
+    static void MyMethod()
+    {
+      Console.WriteLine("executing MyMethod using main Method");
+    }
+
+    static void Main(string[] args)
+    {
+      MyMethod();
+    }
+  }
+}
