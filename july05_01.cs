4ace7472bc5c8d7667103ba0258572fd06917bd2
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-07 23:35:22 +0530

    Update july05_01.cs

--- july05_01.cs
+++ july05_01.cs
@@ -2,11 +2,11 @@ using System;
  
 public class Program
 {
-    public static void Main(string[] args)
+    public static void Main(string[] args) 
     {
         int size = 3;
 		string Name;
-        Person[] persons = new Person[size];
+        Person[] persons = new Person[size]; // line 9 to 14 defines three names of people from the user and stores them in an array of objects of type Person
  
         for (int i = 0; i < size; i++)
         {
@@ -15,7 +15,7 @@ public class Program
 			};
         }
  
-        for (int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) //looping for to get display of input names(ToString by default egConsole.WriteLine(persons[i].ToString) )
         {
             Console.WriteLine(persons[i]);
         }
@@ -24,9 +24,9 @@ public class Program
  
     public class Person
     {
-        public string Name { get; set; }
- 
-        public override string ToString()
+        public string Name { get; set; } //using get method returns the value of the variable name
+ 					// using set method assigns a value to the name variable
+        public override string ToString() //Name property of type string and override the ToString() method
         {
             return "Name :" + Name;
         }
