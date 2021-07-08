1c3f6c6b0f0db4cb0765ce90b88068dd4c5d8e0b
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-08 17:13:03 +0530

    Update jun30_08.cs

--- jun30_08.cs
+++ jun30_08.cs
@@ -7,21 +7,21 @@ namespace altcaps
        public static void Main(string[] args)
         {
  		
-            string str = "C# is used to develop web apps, desktop apps, mobile apps, games and much more.";
+            string str = "C# is used to develop web apps, desktop apps, mobile apps, games and much more."; //defined the statement
             Console.WriteLine("Original string:   " + str);
             
-            char[] ch = str.ToCharArray();
+            char[] ch = str.ToCharArray(); // seperating the statement in character array for each character
             StringBuilder sb = new StringBuilder(); // referred srikanthpayavula code for stringbuilder
 
             for (int i = 0; i < ch.Length; i++)
             {
-                if (i % 2 == 0)
+                if (i % 2 == 0) //declaring condition for even no words
                 {
-                    ch[i] = char.ToUpper(ch[i]);
+                    ch[i] = char.ToUpper(ch[i]); //if even change the alphabet to camalcase
 
                 }
-               
-                sb.Append(ch[i].ToString());
+               //string.join method not used here
+                sb.Append(ch[i].ToString()); // joining the alphabets to make a whole statement again 
 
             }
 
@@ -29,6 +29,7 @@ namespace altcaps
 		}
 	}
 }
-
+/*Original string:   C# is used to develop web apps, desktop apps, mobile apps, games and much more.
+C# iS UsEd tO DeVeLoP WeB ApPs, DeSkToP ApPs, MoBiLe aPpS, gAmEs aNd mUcH MoRe.*/
         
         
