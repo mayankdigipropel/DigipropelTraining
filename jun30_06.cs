2484414f0a92c3cd60dbb0916c505f374193c758
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-08 17:18:24 +0530

    Update jun30_06.cs

--- jun30_06.cs
+++ jun30_06.cs
@@ -5,18 +5,22 @@ using System;
     {
         public static void Main(string[] args)
         {
-            string str1 = "Today is a fine thrusday morning";
+            string str1 = "Today is a fine thrusday morning"; //defined a statement
             Console.WriteLine("Original string: " + str1);
+            //using a function as test ,for other class to call it
             Console.WriteLine("Position of the word 'is' in the said string: " + test(str1, "is"));
             Console.WriteLine("Position of the word 'fine' in the said string: " + test(str1, "fine"));
             Console.WriteLine("Position of the word 'morning' in the said string: " + test(str1, "morning"));
         }
-        public static int test(string text, string word)
+        public static int test(string text, string word) // method to call our test function
         {
             
-            string[] arry = text.Split(' ');
+            string[] arry = text.Split(' '); // separating the statemnt and putting it in string array seperated by spaces
             return Array.IndexOf(arry, word)+1;
        
         }
     }
-
+    /*Original string: Today is a fine thrusday morning
+Position of the word 'is' in the said string: 2
+Position of the word 'fine' in the said string: 4
+Position of the word 'morning' in the said string: 6/*
