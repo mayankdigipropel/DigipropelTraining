38b9919a7f86784ccc9f0b1d6747c4f99bba5b7e
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-13 16:06:34 +0530

    Update july05_04.cs

--- july05_04.cs
+++ july05_04.cs
@@ -2,7 +2,7 @@ using System;
 // declaring public class Person 
 public class Person
 {
-    int age;
+    public int age;
     public void greet()
     {
         Console.WriteLine("Hello!");
@@ -11,6 +11,12 @@ public class Person
     {
         age = n;
     }
+		public void explain()
+        {
+            Console.WriteLine("\n  I'm explaining on the screen Person Age Is :{0}",age); //declaring as per instructions from snippets
+        }
+
+
 }
     // taking Student and Teacher classes inherit from the Person class
     public class Student : Person
@@ -22,14 +28,14 @@ public class Person
         }
         public void ShowAge()
         {
-        Console.WriteLine("My age is: 23 years old on the screen");  //declaring as per instructions from snippets
+        Console.WriteLine("Student calss :: My age is: {0} years old on the screen",age);  //declaring as per instructions from snippets
         }
     }
     public class Teacher : Person
     {
         public void explain()
         {
-            Console.WriteLine("I'm explaining on the screen"); //declaring as per instructions from snippets
+            Console.WriteLine("\n Teacher class:: I'm explaining on the screen Teacher Age Is :{0}",age); //declaring as per instructions from snippets
         }
     }
     // Main method for output that are defined in public class Person
@@ -37,28 +43,30 @@ public class Person
     {
         public static void Main()
         {
-            Person obj1 =new Person();
+            Person obj1 = new Person();
             obj1.greet();
            
            
-            Student obj2 =new Student();
+            Student obj2 = new Student();
             obj2.greet();
-            obj2.SetAge(25);
+            obj2.SetAge(26);
             obj2.ShowAge();
             obj2.study();
            
-            Teacher obj3 =new Teacher();
-            obj3.explain();
-            obj3.SetAge(34);
+            Person obj3 =new Teacher();
+			obj3.SetAge(34);
+            obj3.explain();   
             obj3.greet();
+			 
         }
     }
     /*
-    Hello!
+   Hello!
 Hello!
-My age is: 23 years old on the screen
+Student calss :: My age is: 26 years old on the screen
 I'm studying on the screen
-I'm explaining on the screen
+
+  I'm explaining on the screen Person Age Is :34
 Hello!
 
     */
