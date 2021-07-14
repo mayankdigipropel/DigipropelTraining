8108a5b011300ad5422f5567a7b64bac05819da7
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-14 15:09:10 +0530

    Update july07_04.cs

--- july07_04.cs
+++ july07_04.cs
@@ -1,30 +1,34 @@
+
 using System;
-    public class Student //defining a class with private variables
+
+
+   public class Student
     {
         private int rollno;
         private string name;
         private string course;
         private int feepaid;
-		private static double servicetax = 12.3; //defined for 2nd condition of service tax
+		//static member to store Service Tax, which is set to 12.3%
+        private static double servicetax = 12.3;
 
-        public Student(int rollno, string name, string course)
-        {   //calling the private var using this.
+        public Student(int rollno, string name, string course) // parameterised constructor defined
+        {
             this.rollno = rollno;
             this.name = name;
             this.course = course;
         }
 
-        public void Payment(int amount) //defined method for feepaid
+        public void Payment(int amount)
         {
             feepaid += amount;
         }
 
-        public void Print()
+        public void PrintStdDetails()
         {
-            Console.WriteLine(rollno);
-            Console.WriteLine(name);
-            Console.WriteLine(course);
-            Console.WriteLine(feepaid);
+            Console.WriteLine("RollNO:{0}",rollno);
+            Console.WriteLine("Name:{0}",name);
+            Console.WriteLine("course: {0}",course);
+            Console.WriteLine("Feepaid: {0}",feepaid);
         }
 
         public int DueAmount
@@ -32,21 +36,22 @@ using System;
 
             get
             {
-                return TotalFee - feepaid;
+                return TotalFee - feepaid; //(call_2- total what is calculated in TotalFee - amount paid i.e 1000)
             }
         }
 
         public int TotalFee
         {
-            get // using condition for feepaid for c# and asp.Net
+            get
             {
-                double total = course == "c#" ? 2000 : 3000;
-				// service tax
-                 total = total + total * servicetax / 100;
+                double total = course == "c#" ? 2000 : 3000; // ternery operator (if course is c# then fee will be 2000 else 3000)
+                // service tax
+                total = total + total * servicetax / 100; // (call_1-suppose if course=asp.net then total is 12.3/100 * 2000 + 2000)
                 return (int) total;
             }
         }
-		public static double  ServiceTax
+
+        public static double  ServiceTax // defined Static servicetax property-getters and setters 
         {
             get
             {
@@ -54,36 +59,42 @@ using System;
             }
             set
             {
-                servicetax = value;
+                servicetax = value; // value = 12.3
             }
-    }
+        }
+    } 
 
-    public class AStudent
+   public class UseStudent2
     {
 
         public static void Main()
         {
 
-            Student obj1 = new Student(648, "Name: Ravi", "Course: c#");//defined for c# student
+            Student obj1 = new Student(45, "Karthik", "asp.net");
             obj1.Payment(1000);
-            obj1.Print();
-            Console.WriteLine(obj1.DueAmount);
+            obj1.PrintStdDetails();
+            Console.WriteLine("Due Amount: {0}",obj1.DueAmount); // will call due amount for asp.net refer comment call_1 at line 49
+	    Console.WriteLine();//for next line
 			
-            Student obj2 =new Student(1231, "Name: Asha", "Course: ASP.Net");//defined for asp.net student
+            Student obj2 = new Student(13, "Mayank", "c#");
             obj2.Payment(1000);
-            obj2.Print();
-     		Console.WriteLine(obj2.DueAmount);
+            obj2.PrintStdDetails();
+            Console.WriteLine("Due Amount: {0}",obj2.DueAmount);
+
+
         }
     }
-	}
-	/*
-	output :648
-Name: Ravi
-Course: c#
-1000
-2369
-1231
-Name: Asha
-Course: ASP.Net
-1000
-2369*/
+/*
+Output:
+RollNO:45
+Name:Karthik
+course: asp.net
+Feepaid: 1000
+Due Amount: 2369
+
+RollNO:13
+Name:Mayank
+course: c#
+Feepaid: 1000
+Due Amount: 1246 
+*/
