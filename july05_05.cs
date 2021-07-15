53e255e4745743dfcd477fe395430e75b7c134fd
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-15 21:26:35 +0530

    Update july05_05.cs

--- july05_05.cs
+++ july05_05.cs
@@ -7,6 +7,7 @@ public class CountWordFile
 		String line;
 		int count = 0;
 		//Opens a file in read mode
+		//streamreader-Implements a TextReader that reads characters from a byte stream in a particular encoding.
 		System.IO.StreamReader file = new System.IO.StreamReader(@"C:\Users\user\Desktop\textfile.txt");
 		//Gets each line till end of file is reached
 		while ((line = file.ReadLine()) != null)
