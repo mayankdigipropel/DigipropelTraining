9ce44ab0bbf706a23a9a9c2569cfbd565570494c
mayankdigipropel <86397714+mayankdigipropel@users.noreply.github.com>
2021-07-15 21:28:50 +0530

    Update july05_08.cs

--- july05_08.cs
+++ july05_08.cs
@@ -5,9 +5,9 @@ class RandomNumberGenerator
     {
     //Random class provides functionality to generate random numbers in C#
       Random random = new System.Random();
-      Console.WriteLine("First Random Number is :" +(random.Next(0,1000))); //will generate randon no ranging 0 to 1000
-      Console.WriteLine("2nd random Number is : "+(random.Next(20,50)));//will generate randon no ranging 20 to 50
-      Console.WriteLine("3rd random Number is : "+(random.Next(10,50)));//will generate randon no ranging 10 to 50
+      Console.WriteLine("First Random Number is :" +(random.Next(0,1000))); //will generate random no ranging 0 to 1000
+      Console.WriteLine("2nd random Number is : "+(random.Next(20,50)));//will generate random no ranging 20 to 50
+      Console.WriteLine("3rd random Number is : "+(random.Next(10,50)));//will generate random no ranging 10 to 50
     }
 }
 /*
